532c5e53b319a2b42f8a2c1e9c850db84610146e
Jacob Shaffer <shaffer_jacob@hotmail.com>
2014-08-01 22:31:32 -0400

    forgot to remove build shell script

--- build.sh
+++
@@ -1,3 +0,0 @@
-g++ -Wall -std=c++11 -o bin/main.o -c src/main.cpp -I./include
-g++ -Wall -std=c++11 -o bin/gl3w.o -c src/gl3w.c -I./include
-g++ -Wall -std=c++11 bin/main.o bin/gl3w.o -lglfw3 -ldl -lGL -lGLU -lX11 -lXi -lXrandr -lXxf86vm -lpthread -lXcursor -o bin/camo-generator
