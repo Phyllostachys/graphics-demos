a3675473588b40dc544e3531e631bb33c45af026
Jacob Shaffer <shaffer_jacob@hotmail.com>
2014-08-07 21:35:14 -0400

    added some camo gen logic and output of initial array as a ppm

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <cstdint>
 #include <cstdlib>
 #include <cmath>
+#include <ctime>
 
 #include "GL/gl3w.h"
 #define GLFW_INCLUDE_GLU
@@ -11,7 +12,16 @@
 #include "shader_util.h"
 
 uint8_t board[600][800];
+bool animate = false;
 
+static const char* ppm_header = {
+    "P3\n"
+    "800 600\n"
+    "255\n"
+};
+#define PPM_BLACK_PIXEL "0 0 0 "
+#define PPM_GREEN_PIXEL "107 142 35 "
+#define PPM_BROWN_PIXEL "135 74 43 "
 enum {
     pxl_black = 0,
     pxl_green,
@@ -34,56 +44,95 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 
 int main(int argc, char* argv[])
 {
-    GLFWwindow* window;
+    // init data
+    srand(time(NULL));
 
-    glfwSetErrorCallback(error_callback);
+    int randColor;
+    std::ofstream ofs;
+    ofs.open("initial_colors.ppm", std::ofstream::out | std::ofstream::app);
+    ofs << ppm_header;
 
-    if (!glfwInit()) {
-        exit(EXIT_FAILURE);
+    // generate data
+    for(int y = 600; y > 0; y--) {
+        for(int x = 800; x > 0; x--) {
+            randColor = rand() % 3;
+            std::cout << randColor << " ";
+            board[y][x] = randColor;
+            switch(randColor) {
+                case pxl_black:
+                    ofs << PPM_BLACK_PIXEL;
+                    break;
+                case pxl_green:
+                    ofs << PPM_GREEN_PIXEL;
+                    break;
+                case pxl_brown:
+                    ofs << PPM_BROWN_PIXEL;
+                    break;
+                default:
+                    break;
+            }
+        }
+        std::cout << std::endl;
+        ofs << std::endl;
     }
+    ofs.close();
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
-    window = glfwCreateWindow(800, 600, "Camo-Generator", NULL, NULL);
-    if (!window) {
+#if 0
+    if (animate == true) {
+        GLFWwindow* window;
+
+        glfwSetErrorCallback(error_callback);
+
+        if (!glfwInit()) {
+            exit(EXIT_FAILURE);
+        }
+
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+        window = glfwCreateWindow(800, 600, "Camo-Generator", NULL, NULL);
+        if (!window) {
+            glfwTerminate();
+            exit(EXIT_FAILURE);
+        }
+
+        if (!gl3wInit()) {
+            exit(EXIT_FAILURE);
+        }
+
+        int major, minor, revision;
+        glfwGetVersion(&major, &minor, &revision);
+        std::cout << "Using OpenGL " << major << "." << minor << "." << revision << std::endl;
+        std::cout << "Is OpenGL 4.3 supported?" << (gl3wIsSupported(4, 3) ? " yes" : " no") << std::endl;
+
+        glfwMakeContextCurrent(window);
+        glfwSetKeyCallback(window, key_callback);
+
+        glViewport(0, 0, 800, 600);
+
+        GLuint renderingProgram = compile_shaders();
+        GLuint vertexArrayObject;
+        glGenVertexArrays(1, &vertexArrayObject);
+        glBindVertexArray(vertexArrayObject);
+
+        while (!glfwWindowShouldClose(window)) {
+            // do rendering here
+            glClearBufferfv(GL_COLOR, 0, background);
+
+            glPointSize(64.0f);
+            glUseProgram(renderingProgram);
+            glDrawArrays(GL_POINTS, 0, 1);
+
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
+
+        glDeleteVertexArrays(1, &vertexArrayObject);
+        glDeleteProgram(renderingProgram);
+        glfwDestroyWindow(window);
         glfwTerminate();
-        exit(EXIT_FAILURE);
+        exit(EXIT_SUCCESS);
+    } else {
+        //
     }
-
-    if (!gl3wInit()) {
-        exit(EXIT_FAILURE);
-    }
-
-    int major, minor, revision;
-    glfwGetVersion(&major, &minor, &revision);
-    std::cout << "Using OpenGL " << major << "." << minor << "." << revision << std::endl;
-    std::cout << "Is OpenGL 4.3 supported?" << (gl3wIsSupported(4, 3) ? " yes" : " no") << std::endl;
-
-    glfwMakeContextCurrent(window);
-    glfwSetKeyCallback(window, key_callback);
-
-    glViewport(0, 0, 800, 600);
-
-    GLuint renderingProgram = compile_shaders();
-    GLuint vertexArrayObject;
-    glGenVertexArrays(1, &vertexArrayObject);
-    glBindVertexArray(vertexArrayObject);
-
-    while (!glfwWindowShouldClose(window)) {
-        // do rendering here
-        glClearBufferfv(GL_COLOR, 0, background);
-
-        //glPointSize(64.0f);
-        glUseProgram(renderingProgram);
-        glDrawArrays(GL_POINTS, 0, 1);
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    glDeleteVertexArrays(1, &vertexArrayObject);
-    glDeleteProgram(renderingProgram);
-    glfwDestroyWindow(window);
-    glfwTerminate();
-    exit(EXIT_SUCCESS);
+#endif
 }
--- src/shader_util.cpp
+++ src/shader_util.cpp
@@ -4,6 +4,25 @@
 #include "GL/gl3w.h"
 #include "shader_util.h"
 #define DEBUG
+#define NO_FILE_SHADERS
+
+static const GLchar vs[] = {
+    "#version 430 core\n"
+    "layout(location = 0) in vec4 position;"
+    "void main(void)"
+    "{"
+    "    gl_Position = position;"
+    "}"
+};
+
+static const GLchar fs[] = {
+    "#version 430 core\n"
+    "out vec4 color;"
+    "void main(void)"
+    "{"
+    "    color = vec4(1.0, 0.0, 0.0, 1.0);"
+    "}"
+};
 
 GLuint compile_shaders(void)
 {
@@ -11,6 +30,9 @@ GLuint compile_shaders(void)
     GLuint fragShader;
     GLuint program;
 
+#ifdef NO_FILE_SHADERS
+    const GLchar* vertexShaderSource = vs;
+#else
     std::ifstream ifs;
     std::string inBuffer;
 
@@ -23,6 +45,7 @@ GLuint compile_shaders(void)
         c = ifs.get();
     }
     const GLchar* vertexShaderSource = inBuffer.c_str();
+#endif
     vertShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertShader);
@@ -33,10 +56,15 @@ GLuint compile_shaders(void)
     glGetShaderInfoLog(vertShader, 1024, &length, logBuffer);
     if (length > 0) { std::cout << logBuffer << std::endl; }
 #endif
+#ifndef NO_FILE_SHADERS
     inBuffer.clear();
     ifs.close();
+#endif
 
     // fragment shader
+#ifdef NO_FILE_SHADERS
+    const GLchar* fragmentShaderSource = fs;
+#else
     ifs.open("frag_shader.frag", std::ifstream::in);
     c = ifs.get();
     while (ifs.good()) {
@@ -45,6 +73,7 @@ GLuint compile_shaders(void)
         c = ifs.get();
     }
     const GLchar* fragmentShaderSource = inBuffer.c_str();
+#endif
     fragShader = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fragShader, 1, &fragmentShaderSource, NULL);
     glCompileShader(fragShader);
@@ -53,8 +82,10 @@ GLuint compile_shaders(void)
     glGetShaderInfoLog(vertShader, 1024, &length, logBuffer);
     if (length > 0) { std::cout << logBuffer << std::endl; }
 #endif
+#ifndef NO_FILE_SHADERS
     inBuffer.clear();
     ifs.close();
+#endif
 
     // create program
     program = glCreateProgram();
